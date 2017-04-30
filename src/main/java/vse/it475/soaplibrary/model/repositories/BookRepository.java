a0f64a4bdd9689078bc2fc238bfadbd7fc826cb1
Ondřej Hartman <ondrejhartman@hotmail.com>
2017-04-30 16:50:53 +0200

    Entity a repository

--- src/main/java/vse/it475/soaplibrary/model/repositories/BookRepository.java
+++ src/main/java/vse/it475/soaplibrary/model/repositories/BookRepository.java
@@ -1,8 +1,8 @@
 package vse.it475.soaplibrary.model.repositories;
 
-import io.spring.guides.gs_producing_web_service.Book;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Component;
+import vse.it475.soaplibrary.model.entities.Book;
 
 /**
  * Created by hofmanix on 29/04/2017.
